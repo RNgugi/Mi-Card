cafa43d50afdb93d10b76c708c22618088d24f8a
RNgugi <reubeenkihiu@gmail.com>
2020-12-17 15:24:34 +0300

    2nd Flutter App more changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
           child: Container(
             height: 100.0,
             width: 100.0,
-            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+            margin: EdgeInsets.only(left: 50.0),
+            padding: ,
             color: Colors.white,
             child: Text('Hello'),
           ),
